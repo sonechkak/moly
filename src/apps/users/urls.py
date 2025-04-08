1aaf5d816677d62a52069be9157e22584db41f38
sonya <sonyakarm@icloud.com>
2025-04-08 20:43:15 +0300

    SCRUM-15-added-tests-for-favs-and-baskets-apps

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -1,18 +1,9 @@
-from django.urls import path
+from django.http import HttpResponse
+from django.urls import path, re_path
 
-from .views import (
-    ProfileUpdateView,
-    ProfileView,
-    ShippingAddressCreateView,
-    ShippingAddressDeleteView,
-    ShippingAddressSetPrimaryView,
-    ShippingAddressUpdateView,
-)
+from .views import *
 
 app_name = "users"
-
-
-from django.http import HttpResponse
 
 
 def hello(request):
@@ -22,24 +13,24 @@ def hello(request):
 
 urlpatterns = [
     # Профиль
-    path(r"^profile/<int:pk>", ProfileView.as_view(), name="profile"),
-    path(r"^profile/<int:pk>/update/", ProfileUpdateView.as_view(), name="profile-update"),
+    re_path(r"^profile/<int:pk>", ProfileView.as_view(), name="profile"),
+    re_path(r"^profile/<int:pk>/update/", ProfileUpdateView.as_view(), name="profile-update"),
     # Адреса доставки
-    path(r"^profile/<int:pk>/address/create/", ShippingAddressCreateView.as_view(), name="address-create"),
-    path(
+    re_path(r"^profile/<int:pk>/address/create/", ShippingAddressCreateView.as_view(), name="address-create"),
+    re_path(
         r"^profile/<int:pk>/address/<int:address_pk>/update/",
         ShippingAddressUpdateView.as_view(),
         name="address-update",
     ),
-    path(
+    re_path(
         r"^profile/<int:pk>/address/<int:address_pk>/delete/",
         ShippingAddressDeleteView.as_view(),
         name="address-delete",
     ),
-    path(
+    re_path(
         r"^profile/<int:pk>/address/<int:address_pk>/set_primary/",
         ShippingAddressSetPrimaryView.as_view(),
         name="address-set-primary",
     ),
-    path(r"^hello/", hello, name="hello"),
+    re_path(r"^hello/", hello, name="hello"),
 ]
