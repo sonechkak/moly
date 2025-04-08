f18dc4db2d8d48741fd8c722cd3bc96025c5a7d2
sonya <sonyakarm@icloud.com>
2025-04-08 20:10:34 +0300

    SCRUM-15-added-tests-for-favs-and-baskets-apps

--- src/tests/tests_apps/favs/test_models.py
+++ src/tests/tests_apps/favs/test_models.py
@@ -4,5 +4,3 @@ import pytest
 @pytest.mark.django_db
 def test_basket_with_products(transactional_db, user, favorite_product):
     assert favorite_product.user == user
-    assert favorite_product.product == product
-    assert str(favorite_product) == product.title
