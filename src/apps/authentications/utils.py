7edd2a0b7c5f625b8a55f1a138de476aacc3d038
sonya <sonyakarm@icloud.com>
2025-04-14 15:19:31 +0300

    SCRUM-45-optimized-authentications/utils

--- src/apps/authentications/utils.py
+++ src/apps/authentications/utils.py
@@ -8,13 +8,9 @@ import qrcode
 def generate_qrcode(mfa_hash):
     """Утилита для генерации QR-кода."""
 
-    try:
-        img = qrcode.make(mfa_hash)
-        buffer = BytesIO()
-        img.save(buffer, format="PNG")
-        img_str = base64.b64encode(buffer.getvalue()).decode("utf-8")
-
-    except Exception as e:
-        raise ValueError(f"Error generating QR code: {e}")
+    img = qrcode.make(mfa_hash)
+    buffer = BytesIO()
+    img.save(buffer, format="PNG")
+    img_str = base64.b64encode(buffer.getvalue()).decode("utf-8")
 
     return img_str
