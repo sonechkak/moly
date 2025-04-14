cf2757fa9fc4e0f4e1f34ddc4bfed6ca96069586
sonya <sonyakarm@icloud.com>
2025-04-14 15:40:03 +0300

    SCRUM-45-optimized-authentications/utils.py

--- src/apps/authentications/utils.py
+++ src/apps/authentications/utils.py
@@ -10,6 +10,7 @@ def generate_qrcode(mfa_hash):
     img = qrcode.make(mfa_hash)
     buffer = BytesIO()
     img.save(buffer, format="PNG")
-    img_str = base64.b64encode(buffer.getvalue()).decode("utf-8")
+    stream = buffer.getvalue()
+    img_str = base64.b64encode(stream).decode("utf-8")
 
     return img_str
