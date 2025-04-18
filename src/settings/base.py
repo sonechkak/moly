77995cc46c75bcc359241c4f8c8054dd90a6c6dc
sonya <sonyakarm@icloud.com>
2025-04-18 18:46:13 +0300

    SCRUM-9-adding-cashe-settings

--- src/settings/base.py
+++ src/settings/base.py
@@ -89,7 +89,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.getenv("REDIS_URL"),
+        "LOCATION": os.getenv("REDIS_URL", "redis://redis:6379/1"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "IGNORE_EXCEPTIONS": True,
