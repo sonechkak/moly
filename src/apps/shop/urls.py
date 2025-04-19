5a88b24cefaa13d77552f1bbe6b36b3dd8cf04b5
Alexander <mesteriis@me.com>
2025-04-19 19:22:18 +0200

    Update urls.py

--- src/apps/shop/urls.py
+++ src/apps/shop/urls.py
@@ -14,7 +14,7 @@ app_name = "shop"
 urlpatterns = [
     path("", Index.as_view(), name="index"),
     path("all_products/", SubCategories.as_view(), name="all_products"),
-    path("category_list/<slug:slug>/", SubCategories.as_view(), name="category_list"),
+    path("category_list/<slug:slug>/", cache_page(60*60)(SubCategories.as_view()), name="category_list"),
     path("product_detail/<slug:slug>/", ProductDetail.as_view(), name="product_detail"),
     path("review/<int:pk>", AddReviewView.as_view(), name="add_review"),
 ]
