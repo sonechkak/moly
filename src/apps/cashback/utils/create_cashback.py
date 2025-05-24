f4b6938d4dd5ca1aa76af2070c99df1feb6425c3
sonya <sonyakarm@icloud.com>
2025-05-24 09:13:02 +0300

    SCRUM-27-added-birthday-bonuses

--- src/apps/cashback/utils/create_cashback.py
+++ src/apps/cashback/utils/create_cashback.py
@@ -1,21 +1,39 @@
 from apps.cashback.enums.cashback_choices import CashbackChoices
+from apps.cashback.enums.cashback_types import CashbackTypeChoices
 from apps.cashback.models import Cashback, CashbackBalance
 from django.contrib import messages
 from django.db import transaction
 
+DEFAULT_AMOUNT = 1000
 
-def create_cashback(request, user, order):
+
+def create_cashback(request, user, order=None, birthday_bonus=False, amount=None):
     """Создание кэшбэка для пользователя."""
 
     with transaction.atomic():
-        amount = int(order.total_cost * 0.01)
-        cashback = Cashback.objects.create(
-            user=user, order=order, amount=amount, cashback_status=CashbackChoices.APPROVED
-        )
-        messages.success(request, f"Кэшбек в размере {amount} скоро зачислится на ваш счет.")
+        if order:
+            amount = int(order.total_cost * 0.01)
+            cashback = Cashback.objects.create(
+                user=user, order=order, amount=amount, cashback_status=CashbackChoices.APPROVED
+            )
+
+        if birthday_bonus and not order:
+            if amount:
+                cashback = Cashback.objects.create(
+                    user=user,
+                    amount=amount,
+                    cashback_status=CashbackChoices.APPROVED,
+                    type=CashbackTypeChoices.BIRTHDAY,
+                )
+            else:
+                cashback = Cashback.objects.create(
+                    user=user,
+                    amount=DEFAULT_AMOUNT,
+                    cashback_status=CashbackChoices.APPROVED,
+                    type=CashbackTypeChoices.BIRTHDAY,
+                )
 
         balance, created = CashbackBalance.objects.get_or_create(user=user)
         balance.total += cashback.amount
         balance.total_cashback_earned += cashback.amount
-        balance.total_cashback_used += order.cashback_amount if order.cashback_used else 0
-        balance.save(update_fields=["total", "total_cashback_earned", "total_cashback_used"])
+        balance.save(update_fields=["total", "total_cashback_earned"])
